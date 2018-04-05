6ee6e30df347480914bd54c929cb91cbc6fcc039
Andre Staltz <andre@staltz.com>
2018-04-05 17:35:45 +0300

    rewrite to use a singleton DHT peer, per process

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 var toPull = require('stream-to-pull-stream');
 var swarm = require('discovery-swarm');
 
+var peer = undefined;
+
 function createPeer(_opts, onError) {
   var swarmOpts = _opts || {};
   swarmOpts.dns = typeof swarmOpts.dns === 'undefined' ? false : swarmOpts.dns;
-
-  var key = swarmOpts.key;
-  if (!key) {
-    onError(new Error('multiserver-dht is missing a `key` config'));
-    return;
-  }
-  delete swarmOpts.key;
 
   var port = swarmOpts.port || 8007;
   delete swarmOpts.port;
 
   var sw = swarm(swarmOpts);
   sw.listen(port);
-  sw.join(key, {}, err => {
-    if (err) {
-      onError(err);
-      sw.close();
-    }
-  });
   return sw;
 }
 
@@ -37,13 +26,24 @@ module.exports = function makePlugin(opts) {
     name: 'dht',
 
     server: function(onConnection, onError) {
-      var peer = createPeer(opts, onError);
-      if (!peer) return;
+      var channel = opts.key;
+      delete opts.key;
+      if (!channel) {
+        onError(new Error('multiserver-dht is missing a `key` config'));
+        return;
+      }
+      if (!peer) peer = createPeer(opts, onError);
+      peer.join(channel, {}, err => {
+        if (err) {
+          onError(err);
+          peer.leave(channel);
+        }
+      });
       peer.on('connection', (stream, info) => {
         onConnection(toPull.duplex(stream), info);
       });
       return function() {
-        peer.close();
+        peer.leave(channel);
       };
     },
 
@@ -52,8 +52,19 @@ module.exports = function makePlugin(opts) {
       ['id', 'dns', 'dht', 'utp', 'tcp', 'port'].forEach(name => {
         copyIfDefined(name, opts, clientOpts);
       });
-      var peer = createPeer(clientOpts, cb);
-      if (!peer) return;
+      var channel = clientOpts.key;
+      delete clientOpts.key;
+      if (!channel) {
+        onError(new Error('multiserver-dht is missing a `key` config'));
+        return;
+      }
+      if (!peer) peer = createPeer(clientOpts, cb);
+      peer.join(channel, {}, err => {
+        if (err) {
+          cb(err);
+          peer.leave(channel);
+        }
+      });
       var connected = false;
       peer.on('connection', (stream, info) => {
         if (!connected) {
