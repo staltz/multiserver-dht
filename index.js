fd4585b56a68f05d4d42f8c71ec8a989bf712929
Andre Staltz <andre@staltz.com>
2018-10-31 13:20:32 +0200

    first experiment with hyperswarm

--- index.js
+++ index.js
@@ -1,67 +1,25 @@
 var pull = require('pull-stream');
 var toPull = require('stream-to-pull-stream');
-var datDefaults = require('dat-swarm-defaults');
-var swarm = require('discovery-swarm');
+var network = require('@hyperswarm/network');
 
-function createPeer(_opts) {
-  var swarmOpts = Object.assign({}, _opts || {});
-  delete swarmOpts.key;
-  delete swarmOpts.keys;
-  swarmOpts.dns = typeof swarmOpts.dns === 'undefined' ? false : swarmOpts.dns;
-
-  var port = swarmOpts.port || 0;
-  delete swarmOpts.port;
-
-  var sw = swarm(datDefaults(swarmOpts));
-  sw.once('error', () => {
-    sw.listen(0);
-  });
-  sw.listen(port);
-  return sw;
-}
-
-function copyIfDefined(propName, origin, destination) {
-  if (typeof origin[propName] !== 'undefined') {
-    destination[propName] = origin[propName];
-  }
-}
-
-function removeChannel(serverPeer, channel) {
-  serverPeer.channels.delete(channel);
-  if (serverPeer.channels.size === 0) {
-    serverPeer.removeListener('connection', serverPeer.listener);
-  }
-  serverPeer.leave(channel);
-}
-
-function hostChannels(onError, serverPeer) {
+function hostChannels(serverNet, serverChannels) {
   return channels => {
-    if (!serverPeer) {
-      var msg = 'Unexpected absence of the DHT server';
-      if (onError) onError(new Error(msg));
-      else console.error(msg);
-      return;
-    }
-
     var newChannels = new Set(channels);
-    var oldChannels = serverPeer.channels;
+    var oldChannels = serverChannels;
 
     // newChannels minus oldChannels => add
     newChannels.forEach(channel => {
       if (!oldChannels.has(channel)) {
-        serverPeer.channels.add(channel);
-        serverPeer.join(channel, {}, err => {
-          if (!err) return;
-          if (onError) onError(err);
-          removeChannel(serverPeer, channel);
-        });
+        serverChannels.add(channel);
+        serverNet.join(Buffer.from(channel), {lookup: true, announce: true});
       }
     });
 
     // oldChannels minus newChannels => remove
     oldChannels.forEach(channel => {
       if (!newChannels.has(channel)) {
-        removeChannel(serverPeer, channel);
+        serverChannels.delete(channel);
+        serverNet.leave(Buffer.from(channel));
       }
     });
   };
@@ -69,8 +27,9 @@ function hostChannels(onError, serverPeer) {
 
 module.exports = function makePlugin(opts) {
   opts = opts || {};
-  var serverPeer = undefined;
-  var clientPeers = new Map();
+  var serverChannels = new Set();
+  var clientNet = undefined;
+  var clientTopicToCb = new Map();
 
   return {
     name: 'dht',
@@ -88,70 +47,61 @@ module.exports = function makePlugin(opts) {
       }
       var channel = opts.key;
       var channelsPStream = opts.keys;
-      if (!serverPeer) {
-        serverPeer = createPeer(opts);
-        serverPeer.listener = (stream, info) => {
-          const s = toPull.duplex(stream);
-          s.meta = 'dht';
-          onConnection(s, info);
-        };
-        serverPeer.channels = new Set();
-        serverPeer.on('connection', serverPeer.listener);
-      }
+
+      var serverNet = network({ephemeral: false});
+      serverNet.on('connection', (socket, _details) => {
+        var stream = toPull.duplex(socket);
+        stream.meta = 'dht';
+        onConnection(stream);
+      });
 
       pull(
         channel ? pull.values([[channel]]) : channelsPStream,
-        pull.drain(hostChannels(onError, serverPeer))
+        pull.drain(hostChannels(serverNet, serverChannels))
       );
 
       return () => {
-        serverPeer.removeListener('connection', serverPeer.listener);
-        serverPeer.channels.forEach(c => serverPeer.leave(c));
-        serverPeer.channels.clear();
+        serverChannels.forEach(c => serverNet.leave(Buffer.from(c)));
+        serverChannels.clear();
       };
     },
 
     client: function(x, cb) {
       var clientOpts = typeof x === 'string' ? this.parse(x) : x;
-      ['id', 'dns', 'dht', 'utp', 'tcp'].forEach(name => {
-        copyIfDefined(name, opts, clientOpts);
-      });
       var channel = clientOpts.key;
-      delete clientOpts.key;
       if (!channel) {
         onError(new Error('multiserver-dht needs a `key` in the address'));
         return;
       }
-      if (!clientPeers.has(channel)) {
-        clientPeers.set(channel, createPeer(clientOpts, cb));
+      if (!clientNet) {
+        clientNet = network({ephemeral: true});
+        clientNet.on('connection', (socket, details) => {
+          if (!details.client) {
+            cb(
+              new Error(
+                'client connection was accidentally a server connection'
+              )
+            );
+            return;
+          }
+          var cb = clientTopicToCb.get(details.peer.topic);
+          if (!cb) {
+            cb(
+              new Error(
+                'no client handler was registered for topic: ' +
+                  (details.peer.topic || 'undefined').toString()
+              )
+            );
+            return;
+          }
+          var stream = toPull.duplex(socket);
+          stream.meta = 'dht';
+          cb(null, stream);
+        });
       }
-      var clientPeer = clientPeers.get(channel);
-      var connected = false;
-      var listener = (stream, info) => {
-        if (!connected) {
-          connected = true;
-          const s = toPull.duplex(stream);
-          s.meta = 'dht';
-          cb(null, s, info);
-        }
-      };
-      var closeOnError = err => {
-        if (err) {
-          clientPeers.delete(channel);
-          clientPeer.removeListener('connection', listener);
-          clientPeer.leave(channel);
-          clientPeer.close();
-          cb(err);
-        }
-      };
-      clientPeer.join(channel, {}, closeOnError);
-      clientPeer.on('connection', listener);
-      clientPeer.on('connection-closed', (conn, info) => {
-        if (connected) {
-          connected = false;
-          closeOnError(new Error('connection lost, channel: ' + channel));
-        }
-      });
+      var topic = Buffer.from(channel);
+      clientTopicToCb.set(topic, cb);
+      clientNet.join(topic);
     },
 
     // MUST be dht:<key>
